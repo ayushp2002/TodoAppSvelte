1addcd4121da507df63eb5dbe9f03eb86a5466e8
Ayush Priyadarshi <31592771+ayushp2002@users.noreply.github.com>
2022-04-14 07:57:12 +0530

    Initial commit

---
+++ src/Filters.svelte
@@ -0,0 +1,75 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+
+    let activeClassAll = true;
+    let activeClassPending = false;
+    let activeClassComplete = false;
+</script>
+
+<main>
+    <button
+        id="btnFilterAll"
+        class:active="{activeClassAll}"
+        on:click={e => {
+                dispatch("filter", { filterType: "all" });
+                activeClassAll = !activeClassAll ? !activeClassAll : activeClassAll;
+                activeClassPending = false;
+                activeClassComplete = false;
+            }}>
+        All
+    </button>
+    <button
+        id="btnFilterPending"
+        class:active="{activeClassPending}"
+        on:click={e => {
+                dispatch("filter", { filterType: "pending" });
+                activeClassPending = !activeClassPending ? !activeClassPending : activeClassPending;
+                activeClassAll = false;
+                activeClassComplete = false;
+            }}>
+        Pending
+    </button>
+    <button
+        id="btnFilterComplete"
+        class:active="{activeClassComplete}"
+        on:click={e => {
+                dispatch("filter", { filterType: "completed" });
+                activeClassComplete = !activeClassComplete ? !activeClassComplete : activeClassComplete;
+                activeClassPending = false;
+                activeClassAll = false;
+            }}>
+        Completed
+    </button>
+</main>
+
+<style>
+    main {
+        display: flex;
+        width: 90%;
+        justify-content: space-evenly;
+    }
+    button {
+        width: 33%;
+        background-color: rgb(10, 48, 0);
+        color: rgb(206, 255, 214);
+        cursor: pointer;
+        border-radius: 5px;
+        outline: none;
+    }
+    button:hover {
+        background-color: rgb(14, 70, 0);
+    }
+    button:active {
+        color: rgb(14, 70, 0);
+        background-color: rgb(206, 255, 214);
+    }
+    .active {
+        color: rgb(14, 70, 0);
+        background-color: rgb(188, 255, 199);
+    }
+    .active:hover {
+        background-color: rgb(164, 247, 178);
+    }
+</style>
---
+++ src/Input.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    export let newTask = "";
+</script>
+
+<main>
+    <input type="text" name="newTask" bind:value={newTask} />
+    <button on:click={() => dispatch("add")}>Add task</button>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+    }
+    input {
+        border: 1px solid black;
+        border-radius: 6px;
+    }
+    button {
+        border-radius: 6px;
+        background-color: rgb(10, 48, 0);
+        color: rgb(206, 255, 214);
+        padding: 10px;
+        cursor: pointer;
+    }
+    button:hover {
+        background-color: rgb(14, 70, 0);
+    }
+    button:active {
+        color: rgb(14, 70, 0);
+        background-color: rgb(206, 255, 214);
+    }
+</style>
